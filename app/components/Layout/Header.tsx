e2573dc3716d636bdbf89b90c17954cc2dcf8fc9
Henry Lepa Malomalo <37941816+hlajax@users.noreply.github.com>
2023-11-26 13:40:41 +0100

    Update Header.tsx

--- app/components/Layout/Header.tsx
+++ app/components/Layout/Header.tsx
@@ -13,7 +13,6 @@ const Header = () => (
         <h2 className="flex-1">
           <a href="/">AFRIK SANTE</a>
         </h2>
-        <Image src="https://static.wixstatic.com/media/cdfbfd_e1b91c24151c4513997bc077513fbdfb~mv2.png" alt="afrik santé logo"/>
        <div>
           <Login />
         </div>
