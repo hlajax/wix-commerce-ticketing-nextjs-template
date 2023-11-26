0aa18ea0737ee960d71b180f48279f90ed639df6
Henry Lepa Malomalo <37941816+hlajax@users.noreply.github.com>
2023-11-26 10:52:57 +0100

    10: 52 Update Shop.tsx

--- app/components/Shop/Shop.tsx
+++ app/components/Shop/Shop.tsx
@@ -11,8 +11,8 @@ export function Shop({ items }: { items: products.Product[] }) {
       >
         <h1 className="uppercase text-3xl sm:text-6xl">Merch</h1>
         <p className="text-sm sm:text-base mx-auto px-8 sm:max-w-[50%] my-10">
-          Nos produits sont bios et contribuent au bien-être et à la bonne santé de tous.<br />
-              Nous vous proposons des crèmes, savons, cafés, thé et plein d'autres produits.
+          Nos produits sont saints et bios, ils feront votre bonheur.<br />
+          Nous vous proposons une panoplie de produits qui vous feront du bien.
         </p>
       </div>
       {items.length ? (
